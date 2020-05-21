2deedfa016a1f964e25d0aca475df752f0298969
mcerrillo1 <mcerrillo1@yahoo.com>
2020-05-21 10:48:45 -0700

    refactor

--- datastore.js
+++ datastore.js
@@ -1,140 +1,70 @@
 const STORE = [
     {
-      question: "What are the different lands throughout the main part of Disneyland?",
+      question: "What kind of steak does Raymond have?",
       answers: [
-        'Adventureland, Fantasyland, Frontierland, Tomorrowland',
-        'Mainstreet, The Hub, The Disneyland Castle, Mickeys Parking structure',
-        'Adventureworld, Fantasyworld, Frontierworld, Tomorrowworld',
-        'EPCOT, Animal Kingdom, Hollywood Studios, Magic Kingdom'
+        'Wagyu',
+        'Ground',
+        'New York',
+        'Filet Mignon'
         
         ],
-      correctAnswer: 'Adventureland, Fantasyland, Frontierland, Tomorrowland',
+      correctAnswer: 'Wagyu',
       //icon: 'image of icon',
       //alt: 'description of icon'
     },
 
     {
-        question: "What was Mickey's first cartoon?",
+        question: "There is only one rule in the fucking jungle. What is it?",
         answers: [
-          'Fantasia',
-          'Steamboat Willie',
-          'Silly Symphony',
-          'Brave Little Tailor'
+          'The lion is always the king',
+          'Young Dragons stay away',
+          'When the lion is hungry, HE EATS!',
+          'Guy Ritchie always makes a cameo'
           
           ],
-        correctAnswer: 'Steamboat Willie',
+        correctAnswer: 'When the lion is hungry, HE EATS!',
         //icon: 'image of icon',
         //alt: 'description of icon'
       },
 
       {
-        question: "In Fantasia, Dance of the Hours, what is the Hippo's name?",
+        question: "Waht did Mickey Pearson study at Oxford??",
         answers: [
-          'Helena Hippo',
-          'Hyacinth Hippo',
-          'Helga Hippo',
-          'Henrietta Hippa'
+          'Relativity and Advanced Physics',
+          'Dramatic Film',
+          'The Dark Art of Horticulture',
+          'Molecular Cell Biology'
           
           ],
-        correctAnswer: 'Hyacinth Hippo',
+        correctAnswer: 'The Dark Art of Horticulture',
         //icon: 'image of icon',
         //alt: 'description of icon'
       },
 
       {
-        question: 'The Mark XLII Autonomous Prehensile Propulsion armor is also known as...?',
+        question: 'What does the Lion do when the Young Dragon was being too persistent',
         answers: [
-          'The Hulkbuster',
-          'The Bleeding Edge Armor',
-          'The Prodigal Son',
-          'The Nanotech Armor'
+          'He gave him a lecture and passed on his wisdom so that they could live in peace',
+          'He decided to share his territory with the Young Dragon',
+          'He took the Young Dragon for a walk and put 5 bullets in the back of his skull',
+          'He ate the Young Dragon'
           
           ],
-        correctAnswer: 'The Prodigal Son',
+        correctAnswer: 'He took the Young Dragon for a walk and put 5 bullets in the back of his skull',
         //icon: 'image of icon',
         //alt: 'description of icon'
       },
 
       {
-        question: 'Which of the following actors is credited for the most Disney animated voices?',
+        question: 'Bonus Point Question: Melvin couldve been cast in Henry Goldings role as Dry Eye and played it better',
         answers: [
-          'Alan Tudyk',
-          'Tara Strong',
-          'Mark Hamill',
-          'Jim Cummings'
+          'True',
+          'False'
           
           ],
-        correctAnswer: 'Jim Cummings',
+        correctAnswer: 'True',
         //icon: 'image of icon',
         //alt: 'description of icon'
       },
 
-      {
-        question: 'Which of the following is the most permiere Disney dining experience?',
-        answers: [
-          "Cinderella's Castle",
-          'Club 33',
-          '21 Royal',
-          'Cafe Orleans'
-          
-          ],
-        correctAnswer: '21 Royal',
-        //icon: 'image of icon',
-        //alt: 'description of icon'
-      },
-
-      {
-        question: 'Today, there is Star Wars land, but what Imagineering concept was supposed to go there, but never did?',
-        answers: [
-          'Disney SEA',
-          'Marvel Land',
-          'Fantasyland Expansion',
-          'Discovery Bay'
-          
-          ],
-        correctAnswer: 'Discovery Bay',
-        //icon: 'image of icon',
-        //alt: 'description of icon'
-      },
-
-      {
-        question: 'What were the original names of the Hyenas in The Lion King?',
-        answers: [
-          'Shenzi, Banzai, Ed',
-          'Shenzi, Kamari, Azizi',
-          'Shenzi Marie Predatora Veldetta Jacquelina Hyena, Kovu, Kion',
-          'Shenzi, Zazu, Ahadi'
-          
-          ],
-        correctAnswer: 'Shenzi, Kamari, Azizi',
-        //icon: 'image of icon',
-        //alt: 'description of icon'
-      },
-
-      {
-        question: 'In what year did the Axiom begin its return to Earth after being away for 700 years after the start of Operation Cleanup?',
-        answers: [
-          '2905',
-          '2705',
-          '2805',
-          '3005'
-          
-          ],
-        correctAnswer: '2805',
-        //icon: 'image of icon',
-        //alt: 'description of icon'
-      },
-
-      {
-        question: 'Which Disney Princess has the fewest amount of lines?',
-        answers: [
-          'Jasmine',
-          'Cinderella',
-          'Snow White',
-          'Aurora'
-          
-          ],
-        correctAnswer: 'Aurora',
-        //icon: 'image of icon',
-        //alt: 'description of icon'
-      },]
+    ];
